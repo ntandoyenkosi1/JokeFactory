ea1b16e86a63e75845381e4545d3b14c222f66df
ntandoyenkosi1 <s.ntando99@gmail.com>
2026-03-20 10:20:48 +0200

    Add project files.

---
+++ JokeFactory/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+string asciiBot = @"
+[ ]
+(o o)
+---| |---
+|_|
+";
+Console.WriteLine(asciiBot);
+Console.WriteLine("BEEP BOOP. I AM JokeFactory 3000.");
+Console.WriteLine("-----------------------------");
+Console.Read();
