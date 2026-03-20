e87f42fc1014e48f35c4b23c403a4a46022af594
ntandoyenkosi1 <s.ntando99@gmail.com>
2026-03-20 10:45:33 +0200

    Added ASCII greeting and 2D jokes array

--- JokeFactory/Program.cs
+++ JokeFactory/Program.cs
@@ -8,4 +8,25 @@ string asciiBot = @"
 Console.WriteLine(asciiBot);
 Console.WriteLine("BEEP BOOP. I AM JokeFactory 3000.");
 Console.WriteLine("-----------------------------");
+
+string[,] jokesDatabase = new string[3, 3]
+{
+{
+"Why do programmers prefer dark mode? Because light attracts bugs.",
+"How many programmers does it take to change a light bulb? None, that's a hardware problem.",
+"There are 10 types of people in the world: those who understand binary, and those who don't."
+},
+{
+"I'm reading a book on anti-gravity. I just can't put it down.",
+"Hi Hungry, I'm Dad.",
+"What do you call a fake noodle? An impasta."
+},
+{
+"What do you call a bear with no teeth? A gummy bear.",
+"Why don't scientists trust atoms? Because they make up everything!",
+"What do you get from a pampered cow? Spoiled milk."
+}
+};
+
+
 Console.Read();
